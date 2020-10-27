37180c30bfbaa347f8159aaeb6712769f9a1f1af
Hritwik Tripathi <tripathi.hritwik@gmail.com>
2020-10-28 02:51:12 +0530

    finished the smart contract
    will think of adding more functionality in sometime..

--- sampark/contracts/sampark.sol
+++ sampark/contracts/sampark.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.4.25 <6.2.0;
 
 import "./openzeppelin-solidity/contracts/math/SafeMath.sol";
 import './openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
- import "./openzeppelin-solidity/contracts/access/Ownable.sol";
+import "./openzeppelin-solidity/contracts/access/Ownable.sol";
 
 
 contract Sampark is ERC721 , Ownable {
@@ -32,7 +32,7 @@ contract Sampark is ERC721 , Ownable {
     Art[] public  allartwork;
 
     mapping (string => bool) _artwork_exist;
-    mapping (uint => address) ArtToOwner;
+    mapping (uint256 => address) public ArtToOwner;
     // Constructor
 
     constructor() ERC721("Art","ART") public {
@@ -44,7 +44,7 @@ contract Sampark is ERC721 , Ownable {
 // returns(uint256 _token_id, string memory _name,string memory _skylink, address _curOwnerAdd)
       require(!_artwork_exist[_artwork]);
       allartwork.push(Art(Name, _artwork, msg.sender));
-      uint id = allartwork.length;
+      uint256 id = allartwork.length;
       ArtToOwner[id] = msg.sender;
       emit NewArt(Name ,_artwork,  msg.sender );
       // uint256 _id = art_work.push(_artwork);
@@ -69,11 +69,11 @@ contract Sampark is ERC721 , Ownable {
 
     }
 
-    // function buy(address _from ,address _to,string memory _token_id) public returns(bool){
+    // function buy(address _from ,address _to,uint256 _token_id) public returns(bool){
     //
-    //   // safeTransferFrom(_from,  _to,  _token_id);
+    //    safeTransferFrom(_from,  _to,  _token_id);
     //
-    //   ArtToOwner[_token_id] = _to;
+    //   // ArtToOwner[_token_id] = _to;
     //
     //   return true;
     // }
@@ -81,6 +81,25 @@ contract Sampark is ERC721 , Ownable {
     // function checkOwner(string memory _token_id) public returns(address){
     //
     //   return ArtToOwner[_token_id];
+    // }
+
+
+    function buy(address from , address to, uint256 token_id) private {
+      _transfer( from,  to,  token_id);
+      ArtToOwner[token_id] = to;
+
+    }
+
+    function checkOwner(uint256 _token_id) public view  returns (address) {
+
+      ownerOf(_token_id);
+      return ArtToOwner[_token_id];
+    }
+
+    // function _transfer(address _from, address _to, uint256 _token_id) private override {
+    //
+    //   ArtToOwner[_token_id] = _to;
+    //   emit Transfer(_from, _to, _token_id);
     // }
 
   }
